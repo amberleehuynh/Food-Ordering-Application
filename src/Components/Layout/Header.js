89eb8cbfca33cfc3879e4b8e28d5d9a483579d33
Amberlee Huynh <amberleehuynh5@gmail.com>
2022-01-18 23:26:39 -0800

    Commit

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from 'react'; // To access React.Fragment
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../Assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = props => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>  {/* CSS class w/ a dash, can't use dot notation */} 
+        <img src={mealsImage} alt='Food!'/> 
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
